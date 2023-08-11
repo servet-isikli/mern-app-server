129a091dfd49584162840a84d98f3a57035b4c02
servet-isikli <servetisikli@yahoo.com>
2023-08-12 00:44:47 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+const { configDotenv } = require("dotenv");
 const express = require("express");
 const app = express();
 const port = 5000;
+configDotenv;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+// Connect MongoDB at default port 27017.
+mongoose.connect(
+  "mongodb://localhost:27017/DB Name",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection: " + err);
+    }
+  }
+);
