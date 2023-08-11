cf5adadcfe12f2415bc54746977fc60c2824d036
servet-isikli <servetisikli@yahoo.com>
2023-08-12 01:36:34 +0200

    update

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 // server
 const express = require("express");
-const app = express();
-const port = 5000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// dotenv
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-//mongoose connection
-const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// mongoose connection
 
 const URL = process.env.CONNECTDB_URL;
 
 mongoose.connect(URL, {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-//listening
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
