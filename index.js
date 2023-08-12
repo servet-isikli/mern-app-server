7e9cbbcf068a3a28d198f5cce6338fb6033527a5
servet-isikli <servetisikli@yahoo.com>
2023-08-12 22:50:13 +0200

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// server
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const userRoute = require("./Router/UserRouter");
-
-require("dotenv").config();
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
 
-// mongoose connection
+app.use(cors());
+app.use(bodyParser.json());
 
-const URL = process.env.CONNECTDB_URL;
-
-mongoose.connect(URL, {
+mongoose.connect(process.env.CONNECTDB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -26,8 +18,6 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
-
-app.use("/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
