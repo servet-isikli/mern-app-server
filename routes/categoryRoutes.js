ea26fa24bd52c8b694dbfa3ec6dc8f3dce6daec4
servet-isikli <servetisikli@yahoo.com>
2023-08-22 19:17:34 +0200

    categoryRoutes

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const categoryController = require("../controllers/categoryController");
+
+// Kategoriye göre ürünleri filtreleme
+router.get("/category/:category", categoryController.getCategoryProducts);
+
+module.exports = router;
