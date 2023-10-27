7ad8fee078c85a32fd30c76fd16bcdf3fd3f1c93
servet-isikli <servetisikli@yahoo.com>
2023-10-27 23:49:24 +0200

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // .env dosyasındaki çevresel değişkenleri yükl
 const app = require("./server"); // Express uygulamasını içe aktar
 const mongoose = require("mongoose"); // Mongoose paketini içe aktar
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Mongoose Bağlantısı
 mongoose
